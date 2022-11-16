6a51589a550ae36822b27f34e0213df8073460db
bkhk06 <bkhk06@sina.com>
2022-11-16 14:46:43 +0800

    rust

---
+++ Rust/ex1/target/debug/deps/ex1-0f7734b84a204180.d
@@ -0,0 +1,5 @@
+f:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-0f7734b84a204180.rmeta: src\main.rs
+
+f:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-0f7734b84a204180.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/ex1/target/debug/deps/ex1-5b42d879b17d1ac3.d
@@ -0,0 +1,5 @@
+f:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-5b42d879b17d1ac3.rmeta: src\main.rs
+
+f:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-5b42d879b17d1ac3.d: src\main.rs
+
+src\main.rs:
