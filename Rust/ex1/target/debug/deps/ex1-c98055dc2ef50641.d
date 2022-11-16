b8ad39879837ebce14b8fb04b96dbcbbdee61494
bkhk06 <bkhk06@sina.com>
2022-11-16 14:57:18 +0800

    rust

---
+++ Rust/ex1/target/debug/deps/ex1-c98055dc2ef50641.d
@@ -0,0 +1,5 @@
+F:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-c98055dc2ef50641.exe: src\main.rs
+
+F:\GitHub\Go_Workspace\Rust\ex1\target\debug\deps\ex1-c98055dc2ef50641.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/ex1/target/debug/ex1.d
@@ -0,0 +1,1 @@
+F:\GitHub\Go_Workspace\Rust\ex1\target\debug\ex1.exe: F:\GitHub\Go_Workspace\Rust\ex1\src\main.rs
